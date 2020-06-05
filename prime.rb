40d1f4b1e1d635f4444a9c0b2109c985af8f7d9e
patrick-rush <patrick.a.rush@gmail.com>
2020-06-04 22:12:05 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'pry'
+
+# first we need a range so that we can see if any of them devide evenly into x
+# then we determine first if x is a negative value
+# if x is not negative, we need to divide x by each number in the range,
+# starting at 1. If any of them (besides one) returns true, statement is false
+
+def prime?(x)
+    range = (1..x-1).to_a
+    if x < 1
+        false
+    else
+        range.one? {|n| x % n == 0}
+    end
+end
