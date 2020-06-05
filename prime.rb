ca6feae0979cf8682e1a0c8bb590a21211491621
bebofofum <bri.boehm@gmail.com>
2020-06-05 14:21:11 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(prime_or_not)
+  # range = [2..(prime_or_not-1)]
+  if prime_or_not <= 1
+    return false
+  elsif prime_or_not == 2
+    return true
+  else
+    is_it_prime = (2..prime_or_not-1).each do |num| return false if prime_or_not % num == 0
+    end
+    true
+  end
+
+end
